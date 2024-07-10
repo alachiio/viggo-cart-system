a74ec1aaa9ef5d2b773fb10885f9440c6c4b7af7
alachiio <eng.omar.alachi@gmail.com>
2024-07-10 19:12:26 +0300

    ignore payment

--- src/components/Cart/CheckoutForm.vue
+++ src/components/Cart/CheckoutForm.vue
@@ -263,10 +263,10 @@ const clearDiscount = () => {
           </div>
 
           <button
-            type="submit"
+            type="button"
             class="mt-6 w-full rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
-            Pay ${{ cartStore.total }}
+            Pay ${{ cartStore.total }} <span class="opacity-50">(Not completed)</span>
           </button>
         </div>
       </Modal>
